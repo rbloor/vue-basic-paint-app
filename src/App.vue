b55b181dff6b4b3c0faeb96a1f56ba5516e08b27
Ryan Bloor <ryanbloor@me.com>
2020-03-28 13:42:19 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <div class="color-guide">
+        <h5>Color Guide</h5>
+        <div class="user user">User</div>
+        <div class="user guest">Guest</div>
+      </div>
+      <!-- Bind the custom directive to the canvas element -->
+      <canvas v-canvas></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+  import canvas from './canvas.directive.js';
+  export default {
+    name: 'app',
+    // Here we register our custom directive
+    directives: {
+      canvas,
+    },
+  };
+</script>
+    
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  canvas {
+    background: navy;
+  }
+  .main {
+    display: flex;
+    justify-content: center;
+  }
+  .color-guide {
+    margin: 20px 40px;
+  }
+  h5 {
+    margin-bottom: 10px;
+  }
+  .user {
+    padding: 7px 15px;
+    border-radius: 4px;
+    color: white;
+    font-size: 13px;
+    font-weight: bold;
+    background: red;
+    margin: 10px 0;
+  }
+  .guest {
+    background: greenyellow;
+    color: black;
+  }
+</style>
